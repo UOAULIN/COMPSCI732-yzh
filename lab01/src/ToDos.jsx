aeaa6ff67f09639ce17d1aa8738088ad69f4ccbd
lazy sheep <71829844+yzhzyyy@users.noreply.github.com>
2023-03-22 11:05:47 +1300

    exercise 10

--- lab01/src/ToDos.jsx
+++ lab01/src/ToDos.jsx
@@ -1,15 +1,16 @@
 import styles from './ToDos.module.css';
 
-function ToDos ({todos, changeIsComplete, onTodoStatusChanged}){
+
+function ToDos ({todos, changeIsComplete, onTodoStatusChanged, HandleRemove}){
     return(
         <div className={styles.container}>
             <ul>
                 {todos.map((todo, index) => (
                     <li key={index} className={todo.isComplete ? styles.liComplete : ""}>
-                        <input type = "checkbox"  defaultChecked = {todo.isComplete} onChange = {(e) => {changeIsComplete(todo, e.target.checked), onTodoStatusChanged(index, e.target.checked)}}/>
+                        <input type = "checkbox"  checked = {todo.isComplete} onChange = {(e) => {changeIsComplete(todo, e.target.checked), onTodoStatusChanged(index, e.target.checked)}}/>
                             {todo.description}
                             {todo.isComplete ? <span>(Done!)</span> : <span></span>}
-                            {/* <button onClick = {()=> }>Remove</button> 删除 */}
+                            <button onClick = {() => HandleRemove(index)} > Remove </button>
                     </li>))
                 }
             </ul>
