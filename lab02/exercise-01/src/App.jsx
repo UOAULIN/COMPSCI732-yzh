a843e45c9052ab81f11e491624fb69c6b6b198cc
Zihui Yang <zyan423@UoA.auckland.ac.nz>
2023-03-27 17:23:15 +1300

    exercise 01~05

---
+++ lab02/exercise-01/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import HomePage from './HomePage';
+import AboutPage from './AboutPage';
+
+function App() {
+
+  return (
+    <React.Fragment>
+      <HomePage />
+      <AboutPage />
+    </React.Fragment>
+  );
+}
+
+export default App;
